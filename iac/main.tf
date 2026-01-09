ff4a7bb110fbbed74b651757f6546c1820e2853d
Belmiro Christo Neto <belmiro.neto@trustly.com>
2026-01-09 19:49:41 -0300

    fix: include zone var

--- iac/main.tf
+++ iac/main.tf
@@ -31,6 +31,8 @@ resource "azurerm_postgresql_flexible_server" "main" {
   backup_retention_days        = 7
   geo_redundant_backup_enabled = false
 
+  zone = "1"
+
   tags = azurerm_resource_group.main.tags
 }
 
